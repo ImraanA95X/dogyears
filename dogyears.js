43a616438c29094f8d4fc2659fe85a345f75646b
ImraanA95X <imraan95@live.co.uk>
2018-03-14 23:42:35 +0000

    Add files via upload

---
+++ dogyears.js
@@ -0,0 +1,9 @@
+const myAge = 20; // My age
+let earlyYears = 2; // First two human years
+earlyYears *= 10.5;
+let laterYears = myAge - 2; // First two human years are accounted for
+laterYears *= 4; // Multiply the remaining human years in age by 4
+let myAgeInDogYears = earlyYears + laterYears; // Total age in dog years
+let myName = 'John';
+'John'.toLowerCase(); // makes all characters lower case
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`); // displays name and age in dog years
